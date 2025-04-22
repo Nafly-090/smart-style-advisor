16bb68dfba0be4c5daf37f110df9a842d05aab05
Naflan <naflan265@gmail.com>
2025-04-22 22:42:38 +0530

    Errror fix

--- src/src/unity/imageHelper.js
+++
@@ -1,7 +0,0 @@
-export const getBase64 = (file) =>
-    new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result.split(',')[1]); // Extract Base64 data
-      reader.onerror = (error) => reject(error);
-    });
